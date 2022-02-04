b6db32acff58567ad399b271674d74b8bcd6059b
arghachdas <arghachandra@technoexponent.com>
2022-02-04 19:44:24 +0530

    Next auth setup with mongodb adaptor done

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,25 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import clientPromise from "../../../lib/mongodb"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  pages: {
+    signIn: "/home" // specifies custom login page
+  },
+  session: {
+    strategy: 'jwt',
+    // maxAge - if you want user to get logged out forcefully after some time
+  },
+  debug: true
+})
