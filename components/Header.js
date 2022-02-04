a9e3514731197c4720241b9893ffd8dec5f4d3de
arghachdas <arghachandra@technoexponent.com>
2022-02-04 21:54:16 +0530

    header done with dark mode

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import HeaderLink from "./HeaderLink";
 import GroupIcon from "@mui/icons-material/Group";
@@ -9,13 +9,36 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import { Avatar } from "@mui/material";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
 
 const Header = () => {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
   return (
-    <header>
+    <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
       {/* left */}
       <div className="flex items-center space-x-2 w-full max-w-xs">
-        <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src="https://rb.gy/bizvqj" width={45} height={45} />
+            ) : (
+              <Image src="https://rb.gy/dpmd9s" width={55} height={55} />
+            )}
+          </>
+        )}
 
         <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
           <SearchRoundedIcon />
@@ -35,6 +58,17 @@ const Header = () => {
         <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
         <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
         <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+        {/* dark mode toggle */}
+        {mounted && (
+          <div className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer 
+          flex-shrink-0 relative ${resolvedTheme === 'dark' ? 'justify-end' : "justify-start"}`}
+            onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+          >
+            <span className="absolute left-0">🌜</span>
+            <motion.div className="w-5 h-5 bg-white rounded-full z-40" layout transition={spring} />
+            <span className="absolute right-0.5">🌞</span>
+          </div>
+        )}
       </div>
     </header>
   );
